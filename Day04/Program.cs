e2bd46661508db45007ef5cd830a81dd94c26153
jsumbowo <joelfredericko.jf@gmail.com>
2023-12-21 14:51:35 +0700

    Making my own calculator program that can read the order of math operation

--- Day04/Program.cs
+++ Day04/Program.cs
@@ -1,14 +1,82 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Xml.XPath;
+
 class Program
 {
 	static void Main()
 	{
-		string[] myInput = { "10", "+", "12", "/", "2", "-", "10" };
-		ArrayUpdate(myInput, 6, 3, 5);
-		foreach(string eachInput in myInput)
+		string[] myInput = { "10", "*", "12", "/", "2" };
+
+		int lengthMyInput = myInput.Length;
+		int mathOpIndex = 1;
+		int maxIndex = myInput.Length - 2;
+
+		if (lengthMyInput == 3)
 		{
-			Console.WriteLine(eachInput);
+			Console.WriteLine("The result is: " + MathCalculation(myInput[0], myInput[2], myInput[1]));
 		}
+		else
+		{
+			while (maxIndex >= 3)
+			{
+				// Console.WriteLine("ini belum dibuat");
+				if (IsMathOrderRight(myInput, mathOpIndex))
+				{
+					double result = MathCalculation(myInput[mathOpIndex - 1], myInput[mathOpIndex + 1], myInput[mathOpIndex]);
+					ArrayUpdate(myInput, result, mathOpIndex, maxIndex);
+				}
+				else
+				{
+					double result = MathCalculation(myInput[mathOpIndex + 1], myInput[mathOpIndex + 3], myInput[mathOpIndex + 2]);
+					ArrayUpdate(myInput, result, mathOpIndex + 2, maxIndex);
+				}
+				maxIndex -= 2 ;
+			}
+			Console.WriteLine("The result is: " + MathCalculation(myInput[0], myInput[2], myInput[1]));
+		}
+	}
+
+	static bool IsMathOrderRight(string[] input, int mathOpIndex)
+	{
+		bool result;
+		if ((input[mathOpIndex] == "+" | input[mathOpIndex] == "-") & (input[mathOpIndex + 2] == "*" | input[mathOpIndex + 2] == "/"))
+		{
+			result = false;
+		}
+		else
+		{
+			result = true;
+		}
+		return result;
+	}
+	static double MathCalculation(string firstNumber, string secNumber, string mathOp)
+	{
+		double firstNum = double.Parse(firstNumber);
+		double secNumb = double.Parse(secNumber);
+		double result;
+		if (mathOp == "+")
+		{
+			result = firstNum + secNumb;
+		}
+		else if (mathOp == "-")
+		{
+			result = firstNum - secNumb;
+		}
+		else if (mathOp == "*")
+		{
+			result = firstNum * secNumb;
+		}
+		else if (mathOp == "/")
+		{
+			result = firstNum / secNumb;
+		}
+		else
+		{
+			result = 0;
+		}
+		return result;
 	}
 
 	static void ArrayUpdate(string[] input, double result, int indexMathOp, int maxIndex)
