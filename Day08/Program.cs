0735e05885a0736b563930ee002a8c34d0669ac8
jsumbowo <joelfredericko.jf@gmail.com>
2023-12-29 14:36:52 +0700

    ExectionHandler Type

---
+++ Day08/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program 
+{
+	static void Main()
+	{
+		
+	}
+}
+
+	//Null Reference Exception
+	//string x = null;
+	//x.Length.Dump();
+
+	//IndexOutOfRangeException
+	//int[] myInt = {1,2,3};
+	//myInt[3].Dump();
+
+	//RuntimeBinderException
+	//dynamic a = "hello";
+	//a = 3;
+	//a = 3.0f;
+	//a.AmbilNilaiSekarangJuga().Dump();
+	
+	//FormatException
+	// string a = "3a";
+	// int result = int.Parse(a);
