e6299d0b8d56da9c87d5cccf2613282538f7d84a
jsumbowo <joelfredericko.jf@gmail.com>
2024-01-17 14:27:52 +0700

    Conditional Compile

---
+++ Day11Diagnostics/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+	static void Main()
+	{
+		#if ANDROID
+		#region android
+		Console.WriteLine("Android");
+		#endregion
+		#elif YANTO
+		Console.WriteLine("YANTO");
+		#elif APPLE
+		Console.WriteLine("Apple");
+		#endif
+		
+	}
+}
