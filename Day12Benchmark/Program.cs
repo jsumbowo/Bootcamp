18fe8c1b923075492c1a8716d04f37c90d3faf37
jsumbowo <joelfredericko.jf@gmail.com>
2024-01-18 14:54:37 +0700

    Exception Handling in Multi Threading

--- Day12Benchmark/Program.cs
+++ Day12Benchmark/Program.cs
@@ -10,33 +10,55 @@ class Program
 		Stopwatch stopwatch = new();
 		Console.WriteLine("Starting Program");
 		stopwatch.Start();
-		Thread tTaskOne = new Thread(TaskOne);
-		Thread tTaskTwo = new Thread(TaskTwo);
+		Thread tTaskOne = new Thread(()=> TaskOne('+'));
+		Thread tTaskTwo = new Thread(()=> TaskTwo('-'));
+		Thread tExecption = new Thread(CatchExecption);
 		
 		tTaskOne.Start();
 		tTaskTwo.Start();
+		tExecption.Start();
 		tTaskOne.Join();
 		tTaskTwo.Join();
+		tExecption.Join();
 		
 		stopwatch.Stop();
 		Console.WriteLine($"\nProgram complete. Elapsed time: {stopwatch.ElapsedMilliseconds}ms");
 	}
-	public static void TaskOne()
+	public static void TaskOne(char ch)
 	
 	{
 		for(int i =0; i<100; i++)
 		
 		{
-			Console.Write('+');
+			Console.Write(ch);
 		}
 	}
-	public static void TaskTwo()
+	public static void TaskTwo(char ch)
 	
 	{
 		for(int i =0; i<100; i++)
 		
 		{
-			Console.Write('-');
+			Console.Write(ch);
+		}
+	}
+	public static void ExecptionMaker()
+	
+	{
+		throw new Exception("Error");
+	}
+	public static void CatchExecption()
+	
+	{
+		try
+		
+		{
+			ExecptionMaker();
+		}
+		catch(Exception)
+		
+		{
+			
 		}
 	}
 }
