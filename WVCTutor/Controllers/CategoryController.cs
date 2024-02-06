7dd29f81ba946e621f2868aabb76779fae2c86fd
jsumbowo <joelfredericko.jf@gmail.com>
2024-02-06 14:06:38 +0700

    CRUD feature

--- WVCTutor/Controllers/CategoryController.cs
+++ WVCTutor/Controllers/CategoryController.cs
@@ -26,12 +26,65 @@ public class CategoryController : Controller
 	{
 		return View();
 	}
+	[HttpPost]
 	public IActionResult Add(Category category)
 	
 	{
-		database.Categories.Add(category);
-		database.SaveChanges();
+		IEnumerable<Category> categories = database.Categories.ToList();
+		if(ModelState.IsValid)
+		{
+			if(categories.FirstOrDefault(c => c.CategoryName.ToLower() == category.CategoryName.ToLower()) is null)
+			{
+				database.Categories.Add(category);
+				database.SaveChanges();
+				return RedirectToAction("Index");
+			}	
+		}
+		return RedirectToAction("Add");
+	}
+	
+	public IActionResult Delete(int? id)
+	
+	{
+		Category? category = database.Categories.ToList().FirstOrDefault(c => c.CategoryId == id);
+		if(category is null)
+		
+		{
+			return RedirectToAction("Index");	
+		}
+		return View(category);
+	}
+	[HttpPost]
+	public async Task<IActionResult> Delete(int id)
+	
+	{
+		Category? delCat = database.Categories.ToList().FirstOrDefault(c => c.CategoryId == id);
+		database.Categories.Remove(delCat);
+		await database.SaveChangesAsync();
 		return RedirectToAction("Index");
 	}
 	
+	public IActionResult Edit(int? id)
+	
+	{
+		Category? category = database.Categories.ToList().FirstOrDefault(c => c.CategoryId == id);
+		if(category is null)
+		
+		{
+			return RedirectToAction("Index");	
+		}
+		return View(category);
+	}
+	[HttpPost]
+	public async Task<IActionResult> Edit(Category category)
+	
+	{
+		if(ModelState.IsValid)
+		{
+			database.Categories.Update(category);
+			await database.SaveChangesAsync();
+			return RedirectToAction("Index");
+		}
+		return RedirectToAction("Edit");
+	}
 }
--- WVCTutor/Models/Product.cs
+++ WVCTutor/Models/Product.cs
@@ -5,6 +5,6 @@ public class Product
 	public int ProductId {get; set;}
 	public string ProductName {get; set;} = null!;
 	public string? Description {get; set;}
-	public Category Category {get; set;}
+	public Category? Category {get; set;}
 	public int CategoryId{get; set;}
 }
